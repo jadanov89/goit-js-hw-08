1afcb8350ec7d8d614668dc5efa4f1363f9f50d9
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2022-07-24 19:52:15 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import{ galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "../css/01-gallery.css"
 // Change code below this line
 
 const paretteContainer = document.querySelector('.gallery');
+
 const createMarkup = createGalleryItems(galleryItems);
 
 paretteContainer.insertAdjacentHTML('beforeend', createMarkup);
 
+
+
 function createGalleryItems(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
-        return `<div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </div>`;
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
     }).join('');
 }
 
-
-paretteContainer.addEventListener('click', galleryGlleryImgClick);
+const lightbox = new SimpleLightbox(".gallery a", {
+  selector: "img",
+  data: "alt",
+  position: "bottom",
+  delay: 250,
+});
